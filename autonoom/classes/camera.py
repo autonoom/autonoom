48f422f74ca049a32c93ebc5fc85538ba367d6f7
W03Fi3 <larsh.hanssen@gmail.com>
2017-04-13 23:34:16 +0200

    Imports and stufzz

--- autonoom/classes/camera.py
+++ autonoom/classes/camera.py
@@ -1,3 +1,10 @@
 #File for computer vision stuff
+import cv2 as cv
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import numpy as np
+
+
 class camera:
     def __init__(self):
