22c6ff1f07030aa5fd792f434ea097a30957b243
W03Fi3 <larsh.hanssen@gmail.com>
2017-05-03 14:43:34 +0200

    camera_shizzle

--- autonoom/classes/camera.py
+++ autonoom/classes/camera.py
@@ -1,62 +1,57 @@
-#File for computer vision stuff
-import cv2 as cv
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
 import numpy as np
-import platform
-from matplotlib import pyplot as plt
+import cv2
+import time
+import urllib
 
 
-class Camera:
-    def __init__(self):
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+    except:
+        pass
 
-        os = platform.system()
-        if os == "Windows":
-            cam = cv.VideoCapture(0)
-            cam.resolution = (1080, 480)
-            cam.framerate = 32
-            cam.rotation = 180
-        elif os == "Linux":
-            cam = PiCamera()
-            cam.resolution = (1080, 480)
-            cam.framerate = 32
-            cam.rotation = 180
-            rawCapture = PiRGBArray(cam, size=(1080, 480))
 
-        time.sleep(0.1)
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
-        # capture frames from the camera
-        for frame in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        # grab the raw NumPy array representing the image, then initialize the $
-        # and occupied/unoccupied text
-            cam = frame.array
 
-            src = np.array([[325, 150], [675, 150], [0, 480], [1020, 480]], np.float32)
-            dst = np.array([[0, 0], [1080, 0], [0, 480], [1080, 480]], np.float32)
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    processed_img = cv2.GaussianBlur(processed_img, (3, 3), 0)
+    vertices = np.array([[10, 500], [10, 300], [700, 200], [1000, 200], [1910, 300], [1910, 500]], np.int32)
+    processed_img = roi(processed_img, [vertices])
+    #                       edges
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 50, 15)
+    draw_lines(processed_img, lines)
+    return processed_img
 
-            M = cv.getPerspectiveTransform(src, dst)
-            warp = cv.warpPerspective(cam.copy(), M, (1080, 480))
-            gray = cv.cvtColor(cam, cv.COLOR_BGR2GRAY)
 
-            edges = cv.Canny(gray, 50, 150, apertureSize = 3)
-            minLineLength = 20
-            maxLineGap = 20
-            lines = cv.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
-            for x1, y1, x2, y2 in lines[0]:
-                cv.line(warp, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-                # cv2.imshow("gray",edges)
-
-                # show the frame
-                cv.imshow("Frame", warp)
-            key = cv.waitKey(1) & 0xFF
-            # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                cv.destroyAllWindows()
+def main():
+    last_time = time.time()
+    stream = urllib.urlopen('http://192.168.42.1:8080/?action=stream')
+    bytes = ''
+    while True:
+        bytes += stream.read(1024)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b + 2]
+            bytes = bytes[b + 2:]
+            frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
+            new_screen = process_img(frame)
+            print('Loop took {} seconds'.format(time.time()-last_time))
+            last_time = time.time()
+            cv2.imshow('window', new_screen)
+            # cv2.imshow('wiqndow2', cap)
+            cv2.imshow('window2', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
                 break
-
-
+main()
 
