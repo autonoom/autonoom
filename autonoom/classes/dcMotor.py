3a1ed587dc439ccec6f120fb82cdaedba6cc7f77
Erwin Castricum <castricumerwin@gmail.com>
2017-05-08 22:33:41 +0200

    updated servoMotor. Included base code

--- autonoom/classes/dcMotor.py
+++ autonoom/classes/dcMotor.py
@@ -3,26 +3,30 @@ import threading
 import RPi.GPIO as GPIO
 import time
 
-MOTOR = 18
+MOTOR = 13
 
 
 class dcMotor(threading.Thread):
     def __init__(self):
-        threading.Thread.__init__(self)
-        self.speed = 11.5
-        self.direction = 0
+        self.speed = 0
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(MOTOR, GPIO.OUT)
         self.motor = GPIO.PWM(MOTOR, 100)
-        self.motor.start(0)
-        self.setSpeed(self.speed) #Set initial speed to 0, else the car would start driving right away.
+        self.motor.start(5)
+        self.setZero()
 
     def __del__(self):
         GPIO.cleanup()
 
     def setSpeed(self, speed):
         #Formula for speed to actual ms, check which direction the application is set to as well
-        self.motor.changeDutyCycle(speed)
+        self.motor.ChangeDutyCycle(speed)
         self.speed = speed
+
+    def setZero(self):
+        zeroSpeed = 12.3
+        self.setSpeed(zeroSpeed)
+
+
 
 
--- autonoom/classes/servoMotor.py
+++ autonoom/classes/servoMotor.py
@@ -1,18 +1,32 @@
 #File for servo motor actions
+import threading
+import RPi.GPIO as GPIO
+import time
+
+SERVO = 18
 
 class servoMotor:
     def __init__(self):
         self.position = 0
         self.steps = 0
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(SERVO, GPIO.OUT)
+        self.servo = GPIO.PWM(SERVO, 100)
+        self.servo.start(5)
         self.zeroPosition() #Set position to zero at start
 
     def turnLeft(self, steps):
-        # Formula for steps to actual ms
+        self.steps -= steps
+        self.servo.ChangeDutyCycle(self.steps)
 
     def turnRight(self, steps):
-        # Formula for steps to actual ms
+        self.steps += steps
+        self.servo.ChangeDutyCycle(self.steps)
 
     def zeroPosition(self):
-        # Fixed ms
+        zeroPosition = 11
+        self.servo.ChangeDutyCycle(zeroPosition)
+        self.steps = zeroPosition
+
 
 
