a14b7f63ae7972cbb0eaeb6a4ca77c3793d61731
Unknown <laansam@live.nl>
2017-06-20 15:46:59 +0200

    zeroPos

--- autonoom/classes/servoMotor.py
+++ autonoom/classes/servoMotor.py
@@ -10,6 +10,7 @@ class servoMotor(threading.Thread):
             self.position = 0
             self.steps = 0
             self.servoPin = servoPin
+            self.zeroPosition = 13.4
             # Setup GPIO
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(self.servoPin, GPIO.OUT)
@@ -22,14 +23,15 @@ class servoMotor(threading.Thread):
         def turnLeft(self, steps): #Turn left with .. steps.
             self.steps -= steps
             self.servo.ChangeDutyCycle(self.steps)
+            self.steps = self.zeroPosition
 
         def turnRight(self, steps): #Turn right with .. steps.
             self.steps += steps
             self.servo.ChangeDutyCycle(self.steps)
+            self.steps = self.zeroPosition
 
         def zeroPosition(self): #set position to 0 .. steps.
-            zeroPosition = 13.4
-            self.servo.ChangeDutyCycle(zeroPosition)
-            self.steps = zeroPosition
+            self.servo.ChangeDutyCycle(self.zeroPosition)
+            self.steps = self.zeroPosition
 
 
