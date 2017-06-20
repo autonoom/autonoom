bf09d553b7db9ac7215c417266edead73496515d
Unknown <kelvin_muller@hotmail.com>
2017-06-20 15:33:13 +0200

    poging 1000 servo

--- autonoom/processingUnit.py
+++ autonoom/processingUnit.py
@@ -18,7 +18,7 @@ class core(threading.Thread):
         self.comProt = comProt(5005) #Give port number to object
         self.servoMotor = servoMotor(18) #PWM pin for the servo is 18
         self.sonicSensor = sonicSensor(23,24,60) #Trigger pin, Echo pin and maxdistance
-        self.dcMotor = dcMotor(13) #PWM pin for the engine is 13
+        #self.dcMotor = dcMotor(13) #PWM pin for the engine is 13
         self.stopFlag = False
         #initialize thread
         threading.Thread.__init__(self)
@@ -27,26 +27,26 @@ class core(threading.Thread):
 
     def run(self):
         while True:
-        #     if self.sonicSensor.isNearObject():
-        #         self.goStop()
-        #         if self.stopFlag is False:
-        #             self.goBackward()
-        #             self.servoMotor.turnLeft(12)
-        #         self.stopFlag = True
-        #         self.goStop()
+            if self.sonicSensor.isNearObject():
+                #self.goStop()
+                if self.stopFlag is False:
+                    #self.goBackward()
+                    self.servoMotor.turnLeft(12)
+                self.stopFlag = True
+                #self.goStop()
             self.stopFlag = False
-            # self.servoMotor.zeroPosition()
+            self.servoMotor.zeroPosition()
 
 
-    def goForward(self):
-        if not self.sonicSensor.isNearObject():
-            self.dcMotor.setSpeed(STANDARDSPEEDFORWARD) #Call setSpeed
-
-    def goBackward(self):
-        self.dcMotor.setSpeed(STANDARDSPEEDBACKWARD) #Call setSpeed
-
-    def goStop(self):
-        self.dcMotor.setZero()  #Call setZero
+    # def goForward(self):
+    #     if not self.sonicSensor.isNearObject():
+    #         self.dcMotor.setSpeed(STANDARDSPEEDFORWARD) #Call setSpeed
+    #
+    # def goBackward(self):
+    #     self.dcMotor.setSpeed(STANDARDSPEEDBACKWARD) #Call setSpeed
+    #
+    # def goStop(self):
+    #     self.dcMotor.setZero()  #Call setZero
 
 
 if __name__ == '__main__':
@@ -55,10 +55,10 @@ if __name__ == '__main__':
     #main.goForward()
     while True:
         if main.comProt.data == 'stop':  # if the telnet connection sends a stop signal. Stop
-            main.goStop()
+            #main.goStop()
             main.servoMotor.zeroPosition()
         if main.comProt.data == 'start':  # if the telnet connection sends a start signal. Start
-            main.goForward()
+            #main.goForward()
             #main.servoMotor.zeroPosition()
         if main.comProt.data is not None:  # Output the data is its not NULL
             print "Received data = " + main.comProt.data
