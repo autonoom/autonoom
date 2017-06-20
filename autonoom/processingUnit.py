449fe42cc90a9dc7f2041507ac7dd81ad48d159d
Unknown <kelvin_muller@hotmail.com>
2017-06-20 14:13:45 +0200

    autonomm
    d

--- autonoom/processingUnit.py
+++ autonoom/processingUnit.py
@@ -65,10 +65,10 @@ if __name__ == '__main__':
     #main.goForward()
     while True:
         if main.comProt.data == 'stop':  # if the telnet connection sends a stop signal. Stop
-            #main.goStop()
-            main.servoMotor.zeroPosition()
+            main.goStop()
+            #main.servoMotor.zeroPosition()
         if main.comProt.data == 'start':  # if the telnet connection sends a start signal. Start
-            #main.goForward()
-            main.servoMotor.zeroPosition()
+            main.goForward()
+            #main.servoMotor.zeroPosition()
         if main.comProt.data is not None:  # Output the data is its not NULL
             print "Received data = " + main.comProt.data
